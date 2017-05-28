66b5f41cbab2b071e5ef9a1270b82e6e1389e210
amitbabhale <amitbabhale@gmail.com>
2017-05-29 01:23:07 +0530

    Config server client

--- config-client/src/main/java/com/example/demo/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -2,12 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
-@EnableConfigServer
+
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 }
