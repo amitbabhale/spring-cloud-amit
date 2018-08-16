903866b98b4bb61f0d5423fef71f442a13621b6f
amitbabhale <amitbabhale@gmail.com>
2018-08-16 09:56:57 +0530

    set1
    Set 1 complete

--- ShoppingCart/src/test/java/com/eq/tdd/ShoppingCartTest.java
+++ ShoppingCart/src/test/java/com/eq/tdd/ShoppingCartTest.java
@@ -1,12 +1,13 @@
 package com.eq.tdd;
 
-import static org.junit.Assert.*;
+import java.util.Collections;
+import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import com.eq.tdd.model.Cart;
 import com.eq.tdd.model.Product;
 import com.eq.tdd.repo.ProductRepository;
 import com.eq.tdd.service.ShoppingCartClient;
@@ -25,13 +26,16 @@ public class ShoppingCartTest {
 	
 	private ProductRepository productRepository;
 	private ShoppingCartClient shoppingCartClient;
-
+	Map<String, Product> cart;
 	/**
 	 * Mocks are created there is no infrastructure created to store data.
 	 * @throws Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		cart = Collections.EMPTY_MAP;
+		//as empty Cart is created. Product class need to be created to fix compile time error.
+		
 		productRepository = Mockito.mock(ProductRepository.class);
 		shoppingCartClient = Mockito.mock(ShoppingCartClient.class);
 	}
@@ -51,18 +55,37 @@ public class ShoppingCartTest {
 	@Test
 	public void positive_shouldGetProductInCart() {
 		
-		//Declare empty shopping cart. 
-		Cart cart = null;
-		
-		//compiler shows error as Cart class does not exist so create a Cart class in Model package
-		
-		//To create a product dove i wrote a builder pattern
-		Product dove = Product.builder.name("Dove").price(39.99).build;
-		
-		//compiler gives error as Product class does not exist so created a product class. 
+		//Declare empty shopping cart. it has to be done in junit setup. 
+
+		//To create a product dove use builder patten and i chose lombok library.
 		//now builder is showing error so i need to add dependency on lombok and add annotation on product class.
+		Product product = Product.builder().name("Dove").price(39.99).build();
 		
+		Mockito.when(addQuantityToProduct(5,product)).thenReturn(product);
 		
+		Product dove = addQuantityToProduct(5, product);
+		cart.put(dove.getName(), dove);
+		
+		Assert.assertEquals(5, product.getQuantity());
+		
+		Mockito.when(calculatePrice()).thenCallRealMethod();
+		
+		Assert.assertEquals(199.95, calculatePrice());
+		
+	}
+
+	private double calculatePrice() {
+		double cost = 0;
+		for (int i = 0 ; i < cart.size() ; i++) {
+			Product p = cart.get(i);
+			cost = cost + (p.getPrice() * p.getQuantity());	
+		}
+		
+		return Math.round(cost/100);
+	}
+
+	private Product addQuantityToProduct(int i, Product product) {		
+		return product.builder().quantity(i).build();
 	}
 
 }
--- ShoppingCart/src/test/java/com/eq/tdd/model/Product.java
+++ ShoppingCart/src/test/java/com/eq/tdd/model/Product.java
@@ -3,10 +3,21 @@
  */
 package com.eq.tdd.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * @author amit
  *
  */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Product {
-
+	private String name;
+	private double price;
+	private int quantity;
 }
